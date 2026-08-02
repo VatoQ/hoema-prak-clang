c60dd316be49a8bed45adf1593c253db324394a2
Jonathan Enns <jonathanenns97@web.de>
2026-08-02 10:32:53 +0200

    refactor vector

--- src/vector.c
+++ src/vector.c
@@ -8,7 +8,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-static void Vector_prepare_target(Vector* target, const size_t dim)
+/**
+ * @brief Check status of a given vector.
+ *
+ * @param[] v Vector to be inspected.
+ * @return 1 if `v->values == NULL` or `v->dim == 0`, otherwise 0
+ */
+int Vector_is_empty(const Vector* v)
+{
+    return (v->values == NULL) || (v->dim == 0);
+}
+
+int Vector_dimension_match(const Vector* u, const Vector* v)
+{
+    return u->dim == v->dim;
+}
+
+void Vector_prepare_target(Vector* target, const size_t dim)
 {
     if (Vector_is_empty(target))
     {
@@ -25,11 +41,6 @@ static void Vector_prepare_target(Vector* target, const size_t dim)
 
     Vector_free(target);
     *target = Vector_zeros(dim);
-}
-
-int Vector_is_empty(const Vector* v)
-{
-    return (v->values == NULL) || (v->dim == 0);
 }
 
 Vector Vector_new(const size_t dim, const double init_val)
@@ -84,9 +95,17 @@ Vector Vector_zeros_like(const Vector* v)
 
 Vector Vector_zeros(const size_t dim)
 {
-    Vector res;
-    res.values = calloc(dim, sizeof(double));
-    res.dim    = dim;
+    Vector res = { 0 };
+    if (dim > 0)
+    {
+        res.values = calloc(dim, sizeof(double));
+        if (!res.values)
+        {
+            Log_log("Error allocating data for Vector_zeros()", LOG_ERROR);
+            return (Vector){ 0 };
+        }
+        res.dim = dim;
+    }
     return res;
 }
 
@@ -109,7 +128,7 @@ int Vector_get_at(double* target, const Vector* v, const size_t index)
     }
     *target = v->values[index];
 
-    return VECTOR_BASIC_SUCCESS;
+    return VECTOR_SUCCESS;
 }
 
 int Vector_set_item(Vector* v, const size_t index, const double val)
@@ -120,7 +139,7 @@ int Vector_set_item(Vector* v, const size_t index, const double val)
         return VECTOR_DIMENSION_ERROR;
     }
     v->values[index] = val;
-    return VECTOR_BASIC_SUCCESS;
+    return VECTOR_SUCCESS;
 }
 
 size_t Vector_get_dim(const Vector* v)
@@ -159,7 +178,7 @@ double Vector_norm(const Vector* v)
 
 int Vector_add(Vector* target, const Vector* v)
 {
-    if (Vector_get_dim(target) != Vector_get_dim(v))
+    if (!Vector_dimension_match(target, v))
     {
         Log_log("Dimension error in Vector_add()", LOG_ERROR);
         return VECTOR_DIMENSION_ERROR;
@@ -171,12 +190,30 @@ int Vector_add(Vector* target, const Vector* v)
         target->values[n] += v->values[n];
     }
 
-    return VECTOR_MATH_SUCCESS;
+    return VECTOR_SUCCESS;
+}
+
+int Vector_dot(double* target, const Vector* u, const Vector* v)
+{
+    if (!Vector_dimension_match(u, v))
+    {
+        Log_log("Dimension error in Vector_sub()", LOG_ERROR);
+        return VECTOR_DIMENSION_ERROR;
+    }
+
+    double s = 0.0;
+
+    for (size_t i = 0; i < u->dim; i++)
+    {
+        s += u->values[i] * v->values[i];
+    }
+    *target = s;
+    return VECTOR_SUCCESS;
 }
 
 int Vector_sub(Vector* target, const Vector* v)
 {
-    if (Vector_get_dim(target) != Vector_get_dim(v))
+    if (!Vector_dimension_match(target, v))
     {
         Log_log("Dimension error in Vector_sub()", LOG_ERROR);
         return VECTOR_DIMENSION_ERROR;
@@ -188,7 +225,7 @@ int Vector_sub(Vector* target, const Vector* v)
         target->values[n] -= v->values[n];
     }
 
-    return VECTOR_MATH_SUCCESS;
+    return VECTOR_SUCCESS;
 }
 
 int Vector_scale(Vector* target, const double lambda)
@@ -197,7 +234,7 @@ int Vector_scale(Vector* target, const double lambda)
     {
         target->values[n] *= lambda;
     }
-    return VECTOR_MATH_SUCCESS;
+    return VECTOR_SUCCESS;
 }
 
 int Vector_broadcast_add(Vector* target, const double a)
@@ -206,21 +243,21 @@ int Vector_broadcast_add(Vector* target, const double a)
     {
         target->values[n] += a;
     }
-    return VECTOR_MATH_SUCCESS;
+    return VECTOR_SUCCESS;
 }
 
-int Vector_gradient(Vector* grad, Vector* x, double (*f)(const Vector*))
+int Vector_broadcast_sub(Vector* target, const double a)
+{
+    return Vector_broadcast_add(target, -a);
+}
+
+int Vector_gradient(Vector* grad, const Vector* x, double (*f)(const Vector*))
 {
     const size_t N   = Vector_get_dim(x);
     const double f_x = f(x);
     Vector_prepare_target(grad, N);
     for (size_t n = 0; n < N; n++)
     {
-        // x->values[n] += EPS;
-        // const double f_x_h = f(x);
-        // x->values[n] -= EPS;
-        // grad->values[n] = (f_x_h - f_x) / EPS;
-        //
         double x_orig = x->values[n];
         x->values[n]  = x_orig + EPS;
         double f_plus = f(x);
@@ -232,16 +269,18 @@ int Vector_gradient(Vector* grad, Vector* x, double (*f)(const Vector*))
         grad->values[n] = (f_plus - f_minux) / (2.0 * EPS);
     }
 
-    return VECTOR_MATH_SUCCESS;
+    return VECTOR_SUCCESS;
 }
 
 int Vector_gradient_maximize(Vector* target,
-                             Vector* x,
+                             const Vector* x,
                              double (*f)(const Vector*),
                              double stepsize)
 {
 
     Vector grad = Vector_new(0, 0.0);
+    Vector_prepare_target(target, x->dim);
+    Vector_copy(target, x);
 
     Vector_gradient(&grad, x, f);
     double norm_grad = Vector_norm(&grad);
@@ -251,8 +290,8 @@ int Vector_gradient_maximize(Vector* target,
     double last_f_of_x = f(x);
     Vector test_step   = Vector_new_copy(x);
     int add_status     = Vector_add(&test_step, &grad);
-    bool once_error    = false;
-    once_error |= add_status;
+    int current_status = 0;
+    current_status += add_status;
     double current_f_of_x = f(&test_step);
 
     int count = 0;
@@ -264,9 +303,9 @@ int Vector_gradient_maximize(Vector* target,
             stepsize *= 0.5;
             Vector_copy(&grad, &grad_tmp);
             Vector_scale(&grad, stepsize);
-            Vector_copy(&test_step, x);
+            Vector_copy(&test_step, target);
 
-            once_error |= Vector_add(&test_step, &grad);
+            current_status += Vector_add(&test_step, &grad);
             current_f_of_x = f(&test_step);
             if (a >= 20)
             {
@@ -277,7 +316,7 @@ int Vector_gradient_maximize(Vector* target,
 
         if (current_f_of_x >= last_f_of_x)
         {
-            once_error |= Vector_add(&test_step, &grad);
+            current_status += Vector_add(&test_step, &grad);
             double longer_f_of_x = f(&test_step);
             if (longer_f_of_x > current_f_of_x)
             {
@@ -286,21 +325,21 @@ int Vector_gradient_maximize(Vector* target,
             }
             else
             {
-                once_error |= Vector_sub(&test_step, &grad);
+                current_status += Vector_sub(&test_step, &grad);
                 Vector_scale(&grad, 0.5);
             }
         }
 
-        Vector_copy(x, &test_step);
+        Vector_copy(target, &test_step);
         last_f_of_x = current_f_of_x;
-        Vector_gradient(&grad, x, f);
+        Vector_gradient(&grad, target, f);
         Vector_norm(&grad);
         Vector_copy(&grad_tmp, &grad);
         Vector_scale(&grad, stepsize);
-        once_error |= Vector_add(&test_step, &grad);
+        current_status += Vector_add(&test_step, &grad);
         current_f_of_x = f(&test_step);
 
-        if (once_error)
+        if (current_status)
         {
             Log_log("A dimension error happened in Vector_gradient_maximize",
                     LOG_ERROR);
@@ -308,23 +347,13 @@ int Vector_gradient_maximize(Vector* target,
         }
         count++;
     }
-    // if (target->dim != 0)
-    // {
-    //     Log_log("Non empty Vector deallocated in Vector_maximize()",
-    //             LOG_WARNING);
-    //     Vector_free(target);
-    // }
     Vector_prepare_target(target, x->dim);
-
-    // target->values = calloc(x->dim, sizeof(double));
-    memcpy(target->values, x->values, x->dim * sizeof(double));
-    // target->dim = x->dim;
-
+    memcpy(target->values, target->values, target->dim * sizeof(double));
     Vector_free(&grad);
     Vector_free(&grad_tmp);
     Vector_free(&test_step);
 
-    return VECTOR_MATH_SUCCESS;
+    return VECTOR_SUCCESS;
 }
 
 static double (*minimize_f)(const Vector*);
@@ -335,7 +364,7 @@ static double neg_wrapper(const Vector* x)
 }
 
 int Vector_gradient_minimize(Vector* target,
-                             Vector* x,
+                             const Vector* x,
                              double (*f)(const Vector*),
                              double stepsize)
 {
