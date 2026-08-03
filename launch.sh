02174d1442abd47c8b73b7b71e99dc55af39c838
Jonathan Enns <jonathanenns97@web.de>
2026-08-03 14:22:56 +0200

    prepare launch* for deletion

--- launch.sh
+++ launch.sh
@@ -1,8 +1,12 @@
-#!/bin/env fish
-gcc -O3 -march=native -ffast-math \ 
+#!/bin/env bash
+gcc -O3 -march=native -ffast-math \
     -Iinclude \
-    src/main.c src/vector.c src/matrix.c src/logging.c \
+    src/main.c \
+    src/prng.c \
+    src/vector.c \
+    src/matrix.c \
+    src/logging.c \
     -o main -lm
 
-time ./main
+./main
 
--- launch_test.sh
+++ launch_test.sh
@@ -2,8 +2,8 @@
 
 gcc -O3 -march=native -ffast-math \
     -Iinclude \
-    tests/test_vector.c \
-    src/matrix.c src/vector.c src/logging.c \
+    tests/test_matrix.c \
+    src/matrix.c src/vector.c src/logging.c src/prng.c \
     -o test -lm
 
 
