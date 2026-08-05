481afc40d1861546351eae30114957a61cf94687
Jonathan Enns <jonathanenns97@web.de>
2026-08-05 19:32:55 +0200

    add eigen solver bench

--- benchmarks/bench_matrix.c
+++ benchmarks/bench_matrix.c
@@ -1,67 +1,60 @@
-#include "../include/logging.h"
+#include <stdbool.h>
 #define PARALLEL_THRESHOLD 1000
 #include "../include/matrix.h"
+#include "performance_tracker.h"
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define runs 5
-#define ignore 1
+typedef struct
+{
+    Matrix* out;
+    const Matrix* in;
+} inverse_args;
+
+void inverse_callback(void* ctx)
+{
+    inverse_args* a = ctx;
+    Matrix_inverse(a->out, a->in);
+}
+
+typedef struct
+{
+    Vector* eigen;
+    const Matrix* M;
+    bool sort;
+
+} eigen_args;
+
+void eigen_callback(void* ctx)
+{
+    eigen_args* a = ctx;
+    Matrix_eigvals(a->eigen, a->M, a->sort);
+}
 
 int main(int argc, char** argv)
 {
-    // Inverse:
-    // M * N * ( 6n )  + M * (2N + 1)
-    // + M * N * (6n)   + M
-    // 2 (N^2 * M * 6) + M (2N + 2)
-    // 12M*N^2 + 2MN+2M
-    // 2M ( 6N^2 + N + 1)
-    // M = N:
-    // 2N(6N^2 + N + 1)
-    const size_t m                  = 800;
-    const size_t n                  = m;
-    const size_t FLOPS              = (size_t)(8.0 / 3.0 * n * n * n);
-    Matrix M                        = Matrix_new_random_symmetric(m);
-    Matrix M_inv                    = Matrix_zeros_like(&M);
-    double runtimes[runs - ignore]  = { 0 };
-    double flops_arr[runs - ignore] = { 0 };
+    const size_t runs   = 6;
+    const size_t ignore = 2;
 
-    printf("Benchmark Matrix_inverse()...\n");
+    const size_t m     = 100;
+    const size_t n     = m;
+    const size_t FLOPS = (size_t)(8.0 / 3.0 * n * n * n);
+    Matrix M           = Matrix_new_random_symmetric(m);
+    Matrix M_inv       = Matrix_zeros_like(&M);
+    inverse_args ia    = { &M_inv, &M };
+    callable_t c       = { inverse_callback, &ia };
 
-    double rt_sum = 0.0;
-    double f_sum  = 0.0;
-    for (int r = 0; r < runs; r++)
-    {
-        struct timespec start, end;
-        clock_gettime(CLOCK_MONOTONIC, &start);
-        Matrix_inverse(&M_inv, &M);
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        long seconds = end.tv_sec - start.tv_sec;
-        long nanos   = end.tv_nsec - start.tv_nsec;
+    track_performance(runs, ignore, FLOPS, m, "Matrix_inverse()", c);
 
-        double elapsed         = seconds + nanos * 1e-9;
-        double flop_per_second = (double)FLOPS / elapsed;
-        printf("Run %d: GFLOP/s = %lf", r, flop_per_second / 1000000000);
-        printf(", Runtime = %lf", r, elapsed);
-        if (r >= ignore)
-        {
-            runtimes[r - ignore]  = elapsed;
-            flops_arr[r - ignore] = flop_per_second;
-            rt_sum += elapsed;
-            f_sum += flop_per_second;
-            printf("\n");
-        }
-        else
-        {
-            printf(" (not in average)\n");
-        }
-    }
-    rt_sum /= (runs - ignore);
-    f_sum /= (runs - ignore);
+    Vector eigenvalues       = Vector_new(m, 0);
+    eigen_args ea            = { &eigenvalues, &M, false };
+    callable_t call          = { eigen_callback, &ea };
+    const size_t eigen_flops = 300 * m * m * m;
 
-    printf("GFLOP/s (average): %lf\n", f_sum / 1000000000);
-    printf("Runtime (average): %lf\n", rt_sum);
+    track_performance(runs, ignore, eigen_flops, m, "Matrix_eigvals()", call);
 
+    Matrix_free(&M_inv);
     return EXIT_SUCCESS;
 }
