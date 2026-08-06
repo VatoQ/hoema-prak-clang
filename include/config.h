2c9bace80a8e29376497dd93f89a8cae7aaf36b3
Jonathan Enns <jonathanenns97@web.de>
2026-08-06 12:08:08 +0200

    add config header

---
+++ include/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Macros that may be overwritten in the project can be defined here.
+
+#define LOG_INFO_THRESHOLD 100
+#define PARALLEL_THRESHOLD 1000
+
+#endif // CONFIG_H
--- include/logging.h
+++ include/logging.h
@@ -1,6 +1,10 @@
 #ifndef MATH_LOG_H
 #define MATH_LOG_H
 
+#include "config.h"
+#include <stdbool.h>
+#include <stddef.h>
+
 typedef enum
 {
     LOG_MODE_NONE,
@@ -25,5 +29,7 @@ typedef enum
 void Log_set_log_mode(LogMode mode, LogVerbosity lv);
 void Log_set_log_file(const char* path);
 void Log_log(const char* msg, RecordType rt);
+LogVerbosity Log_get_verbosity(void);
+bool Log_info_threshold(const size_t count, const size_t object_size);
 
 #endif // MATH_LOG_H
