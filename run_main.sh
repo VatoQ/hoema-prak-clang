91b86e362e01dd0c4702815c8ca766107b1df0b7
Jonathan Enns <jonathanenns97@web.de>
2026-08-07 17:03:51 +0200

    omp flags

--- run_main.sh
+++ run_main.sh
@@ -2,8 +2,12 @@
 
 make
 
+set -x OMP_NUM_THREADS 6
 set -x OMP_PROC_BIND true
 set -x OMP_PLACES cores
+set -x OMP_SCHEDULE static
+set -x OMP_DYNAMIC false
+set -x OMP_WAIT_POLICY active
 
 echo "OMP_NUM_THREADS = $OMP_NUM_THREADS"
 echo "OMP_PROC_BIND   = $OMP_PROC_BIND"
