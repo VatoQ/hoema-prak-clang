c472a6f0b181fd030b34a2921915d9bd26d6a582
Jonathan Enns <jonathanenns97@web.de>
2026-09-05 18:42:05 +0200

    add todos

--- include/real_functions.h
+++ include/real_functions.h
@@ -5,14 +5,17 @@
 
 real_t rfn_abs(real_t x);
 real_t rfn_cos(real_t x);
-real_t rfn_cosh(real_t x);
+// TODO: implement
+// real_t rfn_cosh(real_t x);
 real_t rfn_cotan(real_t x);
 real_t rfn_exp(real_t x);
 real_t rfn_mod(real_t a, real_t m);
-real_t rfn_pow(real_t b, real_t e);
+// TODO: implement
+// real_t rfn_pow(real_t b, real_t e);
 real_t rfn_pow_i(real_t x, int_t n);
 real_t rfn_sin(real_t x);
-real_t rfn_sinh(real_t x);
+// TODO: implement
+// real_t rfn_sinh(real_t x);
 real_t rfn_tan(real_t x);
 
 #endif // __REAL_FUNCTIONS_H__
