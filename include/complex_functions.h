ce8b2157e995fae9a5a9e53f2650daaa66f3f821
Jonathan Enns <jonathanenns97@web.de>
2026-09-05 19:07:55 +0200

    add cfn_pow_i()

--- include/complex_functions.h
+++ include/complex_functions.h
@@ -4,5 +4,6 @@
 #include "config.h"
 
 complex_t cfn_exp(real_t phi);
+complex_t cfn_pow_i(complex_t z, int_t n);
 
 #endif // __COMPLEX_FUNCTIONS_H__
--- src/complex_functions.c
+++ src/complex_functions.c
@@ -5,3 +5,33 @@ complex_t cfn_exp(real_t phi)
 {
     return rfn_cos(phi) + rfn_sin(phi) * I;
 }
+
+complex_t cfn_pow_i(complex_t z, int_t n)
+{
+    if (n < 0 && cabs(z) < EPS) // prevent division by zero in the n < 0 case
+    {
+        return ieee_nan() + ieee_nan() * I;
+    }
+    if (n < 0)
+    {
+        z = 1 / z;
+        n = -n;
+    }
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    complex_t y = 1;
+    while (n > 1)
+    {
+        if (n % 2 == 1)
+        {
+            y *= z;
+            n--;
+        }
+        z *= z;
+        n >>= 1;
+    }
+    return z * y;
+}
